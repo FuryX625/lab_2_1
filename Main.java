ce2ca6b4dec1af4b92959a052f43a27d0052105f
FuryX625 <serafin.m625@gmail.com>
2022-04-05 08:49:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    for(int linia=1;linia<=5;linia++){
+      for(int kolumna=1;kolumna<=5;kolumna++){
+        
+      }
+      
+    }
+  }
+}
