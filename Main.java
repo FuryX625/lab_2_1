fb8e2d02f4ff372efeccda1a5b8c951263e707a3
serafin625 <serafin.m625@gmail.com>
2022-04-05 09:00:01 +0000

    1

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void main(String[] args) {
     for(int linia=1;linia<=5;linia++){
-      for(int kolumna=1;kolumna<=5;kolumna++){
-        
+      for(int kolumna=0;kolumna<linia;kolumna++){
+       System.out.print("*"); 
       }
-      
+      System.out.println(" ");
     }
   }
 }
